20385b8f92932a9546df9f6cd9b5a66adc5d67aa
i-samaniego <isabelle128@revature.net>
2022-10-23 21:17:35 -0700

    commit test

---
+++ js06/chapter/js06a_txt.js
@@ -0,0 +1,14 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Chapter case
+
+      Order Form Code
+      Author: Isabelle Samaniego
+      Date:   October 23, 2022
+
+      Filename: js06a.js
+ */
+
+
+      
